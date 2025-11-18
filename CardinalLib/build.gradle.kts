51ac975510b75a2a57bc8386af1b007bd159e5a3
Daniel Wu <danielw72908@gmail.com>
2025-11-17 21:00:15 -0500

    retry again

--- CardinalLib/build.gradle.kts
+++ CardinalLib/build.gradle.kts
@@ -23,33 +23,87 @@ android {
     }
 }
 
-
-
 repositories {
     google()
     mavenCentral()
-//    maven { url = uri("https://jitpack.io") } added line
 }
 
 dependencies {
     implementation("org.firstinspires.ftc:RobotCore:11.0.0")
     implementation("org.firstinspires.ftc:Hardware:11.0.0")
     implementation("org.firstinspires.ftc:FtcCommon:11.0.0")
-
 }
 
-//publishing {
-//    publications {
-//        create<MavenPublication>("release") {
-//            groupId = "org.example"
-//            artifactId = "CardinalLib"
-//            version = "1.0.0"
+// Optional: Maven publishing for JitPack or local
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "org.example"
+            artifactId = "CardinalLib"
+            version = "1.0.0"
+
+            // Point to the AAR output
+            artifact("$buildDir/outputs/aar/CardinalLib-release.aar")
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
+}
+
+
+//plugins {
+//    id("com.android.library") version "8.3.0"
+//    id("maven-publish")
+//}
 //
-//            // Specify the .aar artifact
-//            artifact("$buildDir/outputs/aar/CardinalLib-release.aar")
+//android {
+//    namespace = "org.lib.cardinallib"
+//    compileSdk = 33
+//
+//    defaultConfig {
+//        minSdk = 24
+//    }
+//
+//    buildTypes {
+//        release {
+//            isMinifyEnabled = false
 //        }
 //    }
-//    repositories {
-//        mavenLocal()
+//
+//    compileOptions {
+//        sourceCompatibility = JavaVersion.VERSION_11
+//        targetCompatibility = JavaVersion.VERSION_11
 //    }
 //}
+//
+//
+//
+//repositories {
+//    google()
+//    mavenCentral()
+////    maven { url = uri("https://jitpack.io") } added line
+//}
+//
+//dependencies {
+//    implementation("org.firstinspires.ftc:RobotCore:11.0.0")
+//    implementation("org.firstinspires.ftc:Hardware:11.0.0")
+//    implementation("org.firstinspires.ftc:FtcCommon:11.0.0")
+//
+//}
+//
+////publishing {
+////    publications {
+////        create<MavenPublication>("release") {
+////            groupId = "org.example"
+////            artifactId = "CardinalLib"
+////            version = "1.0.0"
+////
+////            // Specify the .aar artifact
+////            artifact("$buildDir/outputs/aar/CardinalLib-release.aar")
+////        }
+////    }
+////    repositories {
+////        mavenLocal()
+////    }
+////}
