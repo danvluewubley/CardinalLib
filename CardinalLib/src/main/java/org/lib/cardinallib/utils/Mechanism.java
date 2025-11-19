8640e83439f8fb4ae8ee549fce21fac5194136e6
Daniel Wu <danielw72908@gmail.com>
2025-11-19 14:24:35 -0500

    fixed something

--- CardinalLib/src/main/java/org/lib/cardinallib/utils/Mechanism.java
+++ CardinalLib/src/main/java/org/lib/cardinallib/utils/Mechanism.java
@@ -1,4 +1,4 @@
-package org.lib.cardinallib.utils
+package org.lib.cardinallib.utils;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
