7b25d3fd2676f1d7e70aa9528dd68878ff219bd4
Kyle Zhang <126842205+elykz0@users.noreply.github.com>
2025-11-20 14:46:17 -0500

    constants

---
+++ CardinalLib/src/main/java/org/lib/cardinallib/utils/Constants.java
@@ -0,0 +1,9 @@
+package org.lib.cardinallib.utils;
+
+public class Constants {
+    private double weight;
+    private double length;
+    private double width;
+    private double MAX_SPEED;
+    private double NORMAL_SPEED;
+}
